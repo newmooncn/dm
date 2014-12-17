b64ad5ff821172749da621262f4536b76acf0009
John Wang <johnw@dmems.com>
2014-12-17 23:17:33 +0800

    增加产品库存数量字段 增加最大/最小/库存报警字段 库存存放位置 'loc_pos_code': fields.char('Storage Position Code',size=16), #打印条码标志 'is_print_barcode': fields.boolean('Print barcode label'),

---
+++ dmp_stock/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OGO Process Improvements Module
+#    Copyright (C) 2012 OGO (<http://www.odoogo.com>).
+#
+##############################################################################
+{
+    'name': 'DMP Stock',
+    'version': '1.0',
+    'category': 'Customization',
+    'author': 'DMEMS',
+    'website': 'www.dmems.com',
+    'description': """
+Product Improvements
+=====================
+    """,
+    'depends': ["dmp_base", "product", "sale", "stock", "product_manufacturer", "purchase", "mrp"],
+    'data':[#'security/security.xml',
+         'product_view.xml',
+        'wizard/product_set_printflag.xml',
+    ],
+    'auto_install': False,
+    'installable': True
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ dmp_stock/wizard/product_set_printflag.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from openerp.osv import fields, osv
+from openerp.osv.orm import browse_record, browse_null
+from openerp.tools.translate import _
+
+class confirm_msg(osv.osv_memory):
+    _name = "product.set.printflag"
+    _description = "Set product barcode print flag"
+
+    def set_print_flag(self, cr, uid, ids, context=None):
+        active_ids = context and context.get('active_ids', [])
+        active_model = context and context.get('active_model', [])
+        data =  self.browse(cr, uid, ids, context=context)[0]
+        self.pool.get(active_model).print_barcode(cr, uid, active_ids, context=context)
+        return {'type': 'ir.actions.act_window_close'}
+confirm_msg()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
