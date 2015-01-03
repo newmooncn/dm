d7a274c54c07541eb8fc1fa420ce95badf1f96fd
John Wang <johnw@dmems.com>
2015-01-03 12:49:35 +0800

    Fix the issue, check the analytics fields existing, then when the sale/purchase analytic module not installed, system will not execute code

--- dmp_account/account_analytic.py
+++ dmp_account/account_analytic.py
@@ -188,9 +188,9 @@ class stock_picking(osv.osv):
 
     def _prepare_invoice_line(self, cr, uid, group, picking, move_line, invoice_id, invoice_vals, context=None):
         vals = super(stock_picking, self)._prepare_invoice_line(cr, uid, group, picking, move_line, invoice_id, invoice_vals, context=context)
-        if picking.sale_id and move_line.sale_line_id:
+        if picking.sale_id and move_line.sale_line_id and hasattr(move_line.sale_line_id, 'analytics_id'):
             vals.update({'analytics_id':move_line.sale_line_id.analytics_id.id})
-        if picking.purchase_id and move_line.purchase_line_id:      
+        if picking.purchase_id and move_line.purchase_line_id and hasattr(move_line.purchase_line_id, 'analytics_id'):      
             vals.update({'analytics_id':move_line.purchase_line_id.analytics_id.id})
         return vals
 
