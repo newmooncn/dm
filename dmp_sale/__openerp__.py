9069e4e964f98736b831736ba629697e5ca4684e
John Wang <johnw@dmems.com>
2015-01-10 01:11:56 +0800

    Improve on sales

---
+++ dmp_sale/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010 OpenERP s.a. (<http://openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'DMP Sale',
+    'version': '1.0',
+    'category': 'Customization',
+    'author': 'DMEMS',
+    'website': 'www.dmems.com',
+    'description': """
+Sale
+=====================
+    * 
+    """,
+    'depends': ["dmp_base", "dmp_crm", "dmp_mto","sale", "sale_stock", "sale_quick_payment", "sale_exceptions"],
+    'data': [
+        'sale_view.xml',
+        'sale_data.xml'
+    ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
