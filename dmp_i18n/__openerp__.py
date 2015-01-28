fc309a2755579ec4586077bc113963b2684c956f
bjwsz <bjwsz@163.com>
2015-01-28 19:10:49 +0800

    Add dmp_i18n for i18n fixing

---
+++ dmp_i18n/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OGO Process Improvements Module
+#    Copyright (C) 2012 OGO (<http://www.odoogo.com>).
+#
+##############################################################################
+{
+    'name': 'DMP i18n fix',
+    'version': '1.0',
+    'category': 'Customization',
+    'author': 'DMEMS',
+    'website': 'www.dmems.com',
+    'description': """
+i18n fix
+    """,
+
+    'depends': ['base'],
+    'data':[],
+    'auto_install': False,
+    'installable': True
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
