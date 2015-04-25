4f678239d49db474f5e1e9ad7e22e614f834a859
bjwsz <bjwsz@163.com>
2015-04-25 18:18:47 +0800

    +++出入库/调拨单+++ ①　partial数量不能大于stock_move.product_qty +++出库单/调拨单+++ 以下都是针对stock_picking.move_type='direct' ①　产品明细(stock_move): 增加"可用数量","缺少数量", 库存不够使用粗体红色警示 注意:此处可用数量为确认picking,检查可用时候的的可用数量,随着库存变化此数量会不准,最后执行出库界面上面显示的和此处可能不同 ②　for stock_picking ,至少有一个move的库存足够用了,现在状态才变为可用,可以进行出库 改为只要存在stock_move,有可用数量(>0), picking的状态就变为"可用", 用户可进行部分出库 ③　picking在"可用"状态, 如果有部分move为不可用,增加"检查可用"按钮 ④　执行出库界面(partial picking) 不够可以部分出库,现在不能,改为: 打开partial picking的界面时,如果picking有不可用的move,对相关move进行action_assign(),这样会更新move的可用数量和缺少数量 在partial picking界面增加“可用数量"(来自move.可用数量),默认出库数量=min(可用数量,move.product_qty) 库存不够使用粗体红色警示 用户最终partial picking数量不能大于可用数量和move.product_qty

---
+++ dmp_stock_partial_pick/stock.py
@@ -0,0 +1,132 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import netsvc
+from openerp.osv import fields,osv
+import openerp.addons.decimal_precision as dp
+          
+class stock_move(osv.osv):
+    _inherit = "stock.move" 
+        
+    _columns = {   
+        'quantity_out_available' : fields.float("Avail Quantity", digits_compute=dp.get_precision('Product Unit of Measure'), readonly=True),
+        'quantity_out_missing' : fields.float("Missing Quantity", digits_compute=dp.get_precision('Product Unit of Measure'), readonly=True),
+    } 
+                   
+    '''
+    Improve the product reserve logic, to update the quantity_out_available and quantity_out_missing
+    '''
+    def check_assign(self, cr, uid, ids, context=None):
+        """ Checks the product type and accordingly writes the state.
+        @return: No. of moves done
+        """
+        done = []
+        count = 0
+        pickings = {}
+        if context is None:
+            context = {}
+        for move in self.browse(cr, uid, ids, context=context):
+            if move.product_id.type == 'consu' or move.location_id.usage == 'supplier':
+                if move.state in ('confirmed', 'waiting'):
+                    done.append(move.id)
+                pickings[move.picking_id.id] = 1
+                continue
+            if move.state in ('confirmed', 'waiting'):
+                # Important: we must pass lock=True to _product_reserve() to avoid race conditions and double reservations
+                #johnw, 04/25/2014, Improve the product reserve logic, to update the quantity_out_available and quantity_out_missing
+                #res = self.pool.get('stock.location')._product_reserve(cr, uid, [move.location_id.id], move.product_id.id, move.product_qty, {'uom': move.product_uom.id}, lock=True)
+                #if res:
+                res, qty_missing, total_avail = self.pool.get('stock.location')._product_out_avail(cr, uid, [move.location_id.id], move.product_id.id, move.product_qty, {'uom': move.product_uom.id}, lock=True)
+                if total_avail < 0:
+                    total_avail = 0
+                self.write(cr, uid, [move.id], {'quantity_out_available':total_avail, 'quantity_out_missing':move.product_qty-min(total_avail,move.product_qty)})
+                if qty_missing <= 0:
+                    #_product_available_test depends on the next status for correct functioning
+                    #the test does not work correctly if the same product occurs multiple times
+                    #in the same order. This is e.g. the case when using the button 'split in two' of
+                    #the stock outgoing form
+                    self.write(cr, uid, [move.id], {'state':'assigned'})
+                    done.append(move.id)
+                    pickings[move.picking_id.id] = 1
+                    r = res.pop(0)
+                    product_uos_qty = self.pool.get('stock.move').onchange_quantity(cr, uid, [move.id], move.product_id.id, r[0], move.product_id.uom_id.id, move.product_id.uos_id.id)['value']['product_uos_qty']
+                    cr.execute('update stock_move set location_id=%s, product_qty=%s, product_uos_qty=%s where id=%s', (r[1], r[0],product_uos_qty, move.id))
+
+                    while res:
+                        r = res.pop(0)
+                        product_uos_qty = self.pool.get('stock.move').onchange_quantity(cr, uid, [move.id], move.product_id.id, r[0], move.product_id.uom_id.id, move.product_id.uos_id.id)['value']['product_uos_qty']
+                        move_id = self.copy(cr, uid, move.id, {'product_uos_qty': product_uos_qty, 'product_qty': r[0], 'location_id': r[1]})
+                        done.append(move_id)
+                else:
+                    if total_avail > 0:
+                        count += 1
+                        pickings[move.picking_id.id] = 1                    
+                    
+        if done:
+            count += len(done)
+            self.write(cr, uid, done, {'state': 'assigned'})
+
+        if count:
+            for pick_id in pickings:
+                wf_service = netsvc.LocalService("workflow")
+                wf_service.trg_write(uid, 'stock.picking', pick_id, cr)
+        return count
+          
+class stock_picking(osv.osv):
+    _inherit = "stock.picking" 
+    def _partial_assigned(self, cr, uid, ids, field_names, arg, context=None):
+        if not ids: return {}
+        res = {}
+        for pick in self.browse(cr, uid, ids, context=context):
+            res[pick.id] = False
+            for move in pick.move_lines:
+                #partial missing
+                if move.quantity_out_missing > 0 and move.product_qty > move.quantity_out_missing:
+                    res[pick.id] = True
+                    break
+        return res
+    _columns = {   
+        'partial_assigned' : fields.function(_partial_assigned, string="Partial Assigned", type = "boolean", readonly=True),
+    }   
+    '''
+    Improve confirmed-->assigned logic, if the move is partial available(quantity_out_available>0), also make the picking state to assigned
+    '''                         
+    def test_assigned(self, cr, uid, ids):
+        """ Tests whether the move is in assigned state or not.
+        @return: True or False
+        """
+        #TOFIX: assignment of move lines should be call before testing assigment otherwise picking never gone in assign state
+        ok = True
+        for pick in self.browse(cr, uid, ids):
+            mt = pick.move_type
+            # incomming shipments are always set as available if they aren't chained
+            if pick.type == 'in':
+                if all([x.state != 'waiting' for x in pick.move_lines]):
+                    return True
+            for move in pick.move_lines:
+                if (move.state in ('confirmed', 'draft')) and (mt == 'one'):
+                    return False
+                #Improve confirmed-->assigned logic, if the move is partial available(quantity_out_available>0), also make the picking state to assigned
+                #if (mt == 'direct') and (move.state == 'assigned') and (move.product_qty):
+                if (mt == 'direct') and (move.state == 'confirmed') and (move.product_qty) and (move.quantity_out_available):
+                    return True
+                ok = ok and (move.state in ('cancel', 'done', 'assigned'))
+        return ok
---
+++ dmp_stock_partial_pick/stock_partial_picking.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-TODAY OpenERP SA (<http://openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
+from openerp.tools.translate import _
+
+class stock_partial_picking_line(osv.TransientModel):
+    _inherit = "stock.partial.picking.line"
+    _columns = {
+        'quantity_max' : fields.float("Max Quantity", digits_compute=dp.get_precision('Product Unit of Measure'), readonly=True),
+        'quantity_out_available' : fields.float("Avail Quantity", digits_compute=dp.get_precision('Product Unit of Measure'), readonly=True),
+        'pick_type': fields.selection([('out', 'Sending Goods'), ('in', 'Getting Goods'), ('internal', 'Internal')], 'Shipping Type', readonly=True),
+        'state': fields.char('Move State', size=8)
+    }
+
+class stock_partial_picking(osv.osv_memory):
+    _inherit = "stock.partial.picking"
+    
+    def default_get(self, cr, uid, fields, context=None):
+        if context is None: context = {}
+        #do the stock move available checking again, if there are any not ready fully. 
+        picking_id = context.get('active_id')
+        if picking_id:
+            pick = self.pool.get('stock.picking').browse(cr, uid, picking_id, context=context)
+            move_ids = [x.id for x in pick.move_lines if x.state == 'confirmed']
+            if move_ids:
+                self.pool.get('stock.move').action_assign(cr, uid, move_ids)      
+        return super(stock_partial_picking, self).default_get(cr, uid, fields, context=context)   
+            
+    def _partial_move_for(self, cr, uid, move, context=None):
+        partial_move = super(stock_partial_picking, self)._partial_move_for(cr, uid, move)
+        #for the internal/out picking, need control the available out quantity        
+        if move.picking_id.type in('internal', 'out'):
+            quantity = min(partial_move['quantity'], move.quantity_out_available)
+            partial_move.update({'quantity': quantity,'quantity_out_available':move.quantity_out_available})
+            
+        #the max quantity that user can deliver and the picking type
+        partial_move.update({'quantity_max':move.product_qty, 'pick_type':move.picking_id.type, 'state':move.state})
+        return partial_move
+
+    def do_partial(self, cr, uid, ids, context=None):
+        partial = self.browse(cr, uid, ids[0], context=context)
+        picking_type = partial.picking_id.type
+        for wizard_line in partial.move_ids:
+            if wizard_line.quantity <= 0:
+                raise osv.except_osv(_('Error!'), _('[%s]%s, quantity %s must be greater than zero!') % (wizard_line.product_id.default_code, wizard_line.product_id.name, wizard_line.quantity))
+            #user deliver quantity can not be larger than the stock move's original quantity
+            if wizard_line.quantity > wizard_line.quantity_max:
+                raise osv.except_osv(_('Error!'), _('[%s]%s, quantity %s is larger than the original quantity %s') % (wizard_line.product_id.default_code, wizard_line.product_id.name, wizard_line.quantity,  wizard_line.quantity_max))
+            #for the internal/out picking, the  deliver quantity can not be larger than the available quantity
+            if picking_type in('internal', 'out') and wizard_line.quantity > wizard_line.quantity_out_available:
+                raise osv.except_osv(_('Error!'), _('[%s]%s, quantity %s is larger than the available quantity %s') % (wizard_line.product_id.default_code, wizard_line.product_id.name, wizard_line.quantity,  wizard_line.quantity_out_available))
+            
+        return super(stock_partial_picking,self).do_partial(cr, uid, ids, context=context)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
