64b9c4cb857b7d7ff8f4726b8ac0d780eac88db7
bjwsz <bjwsz@163.com>
2015-05-06 22:29:24 +0800

    Improve the product's parent BOM listing

--- dmp_mrp_bom_product/bom_product.py
+++ dmp_mrp_bom_product/bom_product.py
@@ -30,7 +30,7 @@ import os
 
 class product_product(osv.osv):
     _inherit = 'product.product'  
-    def _parent_products(self, cr, uid, ids, field_names, args, context=None):
+    def _parent_boms(self, cr, uid, ids, field_names, args, context=None):
         bom_obj = self.pool.get('mrp.bom')
         res = {} 
         for id in ids:
@@ -40,12 +40,12 @@ class product_product(osv.osv):
 #            bom_ids = bom_obj.search(cr, uid, [('product_id', '=', product_id)])
         for bom in bom_obj.browse(cr, uid, bom_ids, context=context):
             if bom.bom_id:
-                res[bom.product_id.id].append(bom.bom_id.product_id.id)
+                res[bom.product_id.id].append(bom.id)
                 
         return res
             
     _columns={
-              'parent_product_ids': fields.function(_parent_products, type='one2many', relation="product.product", string='Parent Products'),
+              'parent_product_ids': fields.function(_parent_boms, type='one2many', relation="mrp.bom", string='Parent BOMs'),
               }
     def create(self, cr, uid, vals, context=None):
         #if importing need to find direct bom and no bom lines, then system will search the existing bom, and set the direct_bom_id
@@ -53,5 +53,13 @@ class product_product(osv.osv):
             direct_bom_id = self._bom_find(cr, uid, vals['product_id'], None, properties=None)
             if direct_bom_id:
                 vals.update({'direct_bom_id':direct_bom_id})
-        return super(mrp_bom,self).create(cr, uid, vals, context)
+        return super(product_product,self).create(cr, uid, vals, context)
+    
+class mrp_bom(osv.osv):
+    _inherit = 'mrp.bom'             
+    _columns={
+              'bom_product_id': fields.related('bom_id', 'product_id', type='many2one', relation="product.product", string='Parent BOM Product'),
+              'bom_customer_product_name': fields.related('bom_product_id', 'customer_product_name', type='char', string='Parent BOM Customer Product Name'),
+              }
+     
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
