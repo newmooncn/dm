52f88bf273730c43ad6d9c51b8deb8b9349a7301
bjwsz <bjwsz@163.com>
2015-06-11 12:05:06 +0800

    Add remove access restriction module

---
+++ dm_remote/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    DMEMS Base Module
+#    Copyright (C) 2014 DMEMS (<http://www.dmems.com>).
+#
+##############################################################################
+{
+    'name': 'DM Remote Acess',
+    'version': '1.0',
+    'category': 'Customization',
+    'sequence': 1000,
+    'summary': 'DMEMS Remote Acess',
+    'description': """
+Only allow some users access some Host, useful for the internet accessing
+    """,
+    'author': 'DMEMS',
+    'website': 'http://www.dmems.com',
+    'images': [],
+    'depends': ['base'],
+    'data': [],
+    #web
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
