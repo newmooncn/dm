c981b8a6ee866a15446fb9d8d45bb81f693e85b6
John Wang <johnw@dmems.com>
2015-07-24 18:47:38 +0800

    Add dmp_account module for accounting base improvement

---
+++ dmp_account/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    DMEMS Process Improvements Module - Accounting
+#    Copyright (C) 2014 DMEMS (<http://www.dmems.com>).
+#
+##############################################################################
+{
+    'name': 'DMP Account base',
+    'version': '1.0',
+    'category': 'Customization',
+    'author': 'DMEMS',
+    'website': 'www.dmems.com',
+    'description': """
+Accounting Account base
+=====================
+    * allow to delete invoice with draft/cancel, but have been invalidated
+    * improve sales order invoice preparation
+    """,
+
+    'depends': ['dmp_base','account','sale'],
+    'data':[],
+    'auto_install': False,
+    'installable': True
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
