ec94af25f3266c5e527754fcf397586540eb2a49
John Wang <johnw@dmems.com>
2015-11-14 23:48:41 +0800

    Add account active module to set journal's active flag

---
+++ dmp_account_active/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    DMEMS Process Improvements Module - Accounting
+#    Copyright (C) 2014 DMEMS (<http://www.dmems.com>).
+#
+##############################################################################
+{
+    'name': 'DMP Account active flag',
+    'version': '1.0',
+    'category': 'Customization',
+    'author': 'DMEMS',
+    'website': 'www.dmems.com',
+    'description': """
+Accounting Account journal active flag
+=====================
+    * allow to delete invoice with draft/cancel, but have been invalidated
+    * improve sales order invoice preparation
+    * Add account_voucher.invoice_id and account_move.invoice_id for the Invoice Payment
+    """,
+
+    'depends': ['dmp_base','account'],
+    'data':['account_view.xml'],
+    'auto_install': False,
+    'installable': True
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
